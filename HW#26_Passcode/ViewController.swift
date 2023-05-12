c86dad3df13f32422dd63f6262c28bf0539be559
Daweihao84 <dwsamurai84@gmail.com>
2023-05-12 11:46:05 +0800

    Initial Commit

---
+++ HW#26_Passcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW#26_Passcode
+//
+//  Created by Dawei Hao on 2023/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
